f1bb948b856c28d770a56aebccdfab3d8edce5bb
Firvox <firvoxfaust0321@gmail.com>
2014-03-06 19:19:01 +0800

    Create Welcome.cs

---
+++ Welcome.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Welcome
+
+{
+
+  static void Main() {
+  
+    Console.WriteLine("Please enter your name:");
+    
+    Console.ReadLine();
+    
+    Console.WriteLine("Welcome aboard, captain.");
+    
+  }
+  
+}
